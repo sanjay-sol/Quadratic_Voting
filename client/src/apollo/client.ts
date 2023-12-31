b47299146c592924ac63c29049a68ae41d181d23
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-12-31 12:22:16 +0530

    changed localhost var in .env after deploy

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -2,7 +2,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-const client = new ApolloClient({
+const client = new ApolloClient({ 
   uri: process.env.NEXT_PUBLIC_GRAPHQL_API,
   cache: new InMemoryCache(),
 });
